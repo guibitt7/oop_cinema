2d89783f3ed0401da3693ef08a848f5eaaf7810f
Guilherme Bittencourt da Silva <guilherme@MacBook-Air-de-Guilherme.local>
2025-01-22 07:21:17 -0300

    terminando pandas

--- teste.py
+++
@@ -1,8 +0,0 @@
-# filmes = []
-# for filme_elemento in filme_elementos:
-#     nome = filme_elemento.querySelector('seletor_do_nome').innerText
-#     diretor = filme_elemento.querySelector('seletor_do_diretor').innerText
-#     filme = Filme(nome=nome, diretor=diretor)
-#     filmes.append(filme)
-
-# ideia de criacao do playwright
